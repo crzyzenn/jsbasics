8cd68bc89ece27413f7df51e82b83f8b7b45ee79
crzyzenn <zenishshr@gmail.com>
2021-04-23 10:00:08 +0545

    Getting started with Basics

---
+++ script.js
@@ -0,0 +1,39 @@
+// // Declaring a variable
+// // Declare a variable and 
+// // console.log it.
+
+// let name = "Some value" // string
+// let number = 1 // number
+// let eligible = true // boolean
+// let person; // undefined
+// let anotherPerson = null // null
+
+
+// // Create an object
+// person = {
+//   name: 'Zenish',
+//   age: 30,
+// }
+
+// person.address = "Sifal"
+// console.log(person)
+
+
+// let personNames = ['Henry', 'Tom', 'Jerry']
+// console.log(personNames[3])
+
+// Declare a function named multiply
+// function multiply(a, b) {
+//   return a * b
+// }
+
+// let multipliedNumber = multiply(5, 6)
+// console.log(multipliedNumber)
+
+// Function with a default parameter
+function square(a = 2) {
+  return a * a
+}
+
+let squaredNumber = square()
+console.log(squaredNumber)
